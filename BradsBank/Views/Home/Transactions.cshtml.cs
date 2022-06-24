01612f7c2d69b7c190b8c833593fa1ed265e22a9
aalsarori <55166509+aalsarori@users.noreply.github.com>
2022-06-24 15:52:58 -0600

    attempt to git the tables into an array

--- BradsBank/Views/Home/Transactions.cshtml.cs
+++ BradsBank/Views/Home/Transactions.cshtml.cs
@@ -4,6 +4,9 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using System.Data.SqlClient;
+
+
 
 namespace BradsBank.Views.Home
 {
@@ -37,38 +40,88 @@ namespace BradsBank.Views.Home
             }
         }
 
-        public List<List<string>> PartialTransactionHistory(string accountType)
+        public List<string> PartialTransactionHistory(string accountType)
         {
-            var HistoryList = new List<List<string>>();
+            //var HistoryList = new List<List<string>>();
+
+
+            /*var i = 1;
+            var number = new int[60];
+            var items = new string[60];
+
+            SqlCommand command = new SqlCommand(..., connection); ;
+            var dr = command.ExecuteReader(); ;
+
+
+
+
+            while (dr.Read())
+            {
+                number[i] = Convert.ToInt32(dr[0]);
+                items[i] = Convert.ToString(dr[1]);
+                ...
+
+                              i = i + 1;
+            };
+
+            dr.Close();
+            connection.Close();*/
+
+
+            //private readonly ILogger<HomeController> _logger;
+            // private readonly IConfiguration configuration;
+
+            string connectionString = "Server=titan.cs.weber.edu, 10433;Database=AmandaShow;User ID=AmandaShow;Password=+his!$TheP@$$w0rd" ;
+
+            SqlConnection connection = new SqlConnection(connectionString);
+
+            connection.Open();
+
+            var valuesList = new List<string>();
+
+
+            connection.Open();
+            //Read from the database
+
+            string getTable = String.Format("SELECT COUNT(*) FROM Transactoins WHERE Username = '{0}' ", accountType);
+            SqlCommand command = new SqlCommand(getTable, connection);
+
+            SqlDataReader dataReader = command.ExecuteReader();
+
+            while (dataReader.Read())
+            {
+                valuesList.Add((string)dataReader[0]);
+            }
+            connection.Close();
 
             // Get the transaction history data from the Database
 
-                //item[0], item[1], item[2]
+            //item[0], item[1], item[2]
 
-                // HOW IT SHOULD LOOK LIKE
-                // <tr>
-                // <th>Date</th>
-                // <th>Type</th>
-                // <th>Account</th>
-                // <th>Amount</th>
-                // </tr>
+            // HOW IT SHOULD LOOK LIKE
+            // <tr>
+            // <th>Date</th>
+            // <th>Type</th>
+            // <th>Account</th>
+            // <th>Amount</th>
+            // </tr>
 
-                // <tr>
-                // <td>mm/dd/yyyy</td>
-                // <td>Example</td>
-                // <td>Account1</td>
-                // <td>$0.00</td>
-                // </tr>
+            // <tr>
+            // <td>mm/dd/yyyy</td>
+            // <td>Example</td>
+            // <td>Account1</td>
+            // <td>$0.00</td>
+            // </tr>
 
-                // HOW WE WILL RUN IT
-                // <tr>
-                // <td>item[0]</td>
-                // <td>item[1]</td>
-                // <td>item[2]</td>
-                // <td>item[3]</td>
-                // </tr>
+            // HOW WE WILL RUN IT
+            // <tr>
+            // <td>item[0]</td>
+            // <td>item[1]</td>
+            // <td>item[2]</td>
+            // <td>item[3]</td>
+            // </tr>
 
-            return HistoryList;
+            return valuesList;
         }
 
         public void OnGet()
