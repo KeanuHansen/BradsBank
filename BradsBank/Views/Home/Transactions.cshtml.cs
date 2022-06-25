976a32d326da8344e3673d2de6379502b0a56af5
Keanu Hansen <keanu.jon.hansen@gmail.com>
2022-06-24 20:21:49 -0600

    | |

--- BradsBank/Views/Home/Transactions.cshtml.cs
+++ BradsBank/Views/Home/Transactions.cshtml.cs
@@ -36,7 +36,6 @@ namespace BradsBank.Views.Home
         {
             get
             {
-
                 string connectionString = "Server=titan.cs.weber.edu, 10433;Database=AmandaShow;User ID=AmandaShow;Password=+his!$TheP@$$w0rd";
 
                 SqlConnection connection = new SqlConnection(connectionString);
