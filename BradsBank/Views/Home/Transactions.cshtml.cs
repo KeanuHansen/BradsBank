9a802cce1a681bc864352b51c272a15134f9da8f
Keanu Hansen <keanu.jon.hansen@gmail.com>
2022-06-24 20:45:01 -0600

    | Code |

--- BradsBank/Views/Home/Transactions.cshtml.cs
+++ BradsBank/Views/Home/Transactions.cshtml.cs
@@ -42,19 +42,20 @@ namespace BradsBank.Views.Home
 
                 var valuesList = new List<string>();
 
+                var returnList = new List<List<string>>();
 
                 connection.Open();
                 //Read from the database
 
-                string getTable = String.Format("SELECT * FROM Transactions JOIN Account ON Account.Account = Transactions.Account WHERE Account.Username = '{0}'  ", m_User);
+                string getTable = String.Format("SELECT Transactions.BUSINESSDATE, Account.ACCOUNTTYPE, Transactions.AMOUNT, Transactions.TRANSDESC FROM Transactions JOIN Account ON Account.Account = Transactions.Account WHERE Account.Username = '{0}'  ", m_User);
 
                 if (m_Type == "none")
                 {
-                    getTable = String.Format("SELECT * FROM Transactions JOIN Account ON Account.Account = Transactions.Account WHERE Account.Username = '{0}'  ", m_User);
+                    getTable = String.Format("SELECT Transactions.BUSINESSDATE, Account.ACCOUNTTYPE, Transactions.AMOUNT, Transactions.TRANSDESC FROM Transactions JOIN Account ON Account.Account = Transactions.Account WHERE Account.Username = '{0}'  ", m_User);
                 }
                 else
                 {
-                    getTable = String.Format("SELECT * FROM Transactions JOIN Account ON Account.Account = Transactions.Account WHERE Account.AccountType = '{0}' AND Account.Username = '{1}'  ", m_Type, m_User);
+                    getTable = String.Format("SELECT Transactions.BUSINESSDATE, Account.ACCOUNTTYPE, Transactions.AMOUNT, Transactions.TRANSDESC FROM Transactions JOIN Account ON Account.Account = Transactions.Account WHERE Account.AccountType = '{0}' AND Account.Username = '{1}'  ", m_Type, m_User);
                 }
                 SqlCommand command = new SqlCommand(getTable, connection);
 
@@ -62,6 +63,30 @@ namespace BradsBank.Views.Home
 
                 while (dataReader.Read())
                 {
+                    var tempList = new List<string>();
+
+                    // Date
+                    var date = dataReader[0].ToString();
+
+                    // Account Type
+                    var type = dataReader[1].ToString();
+
+                    // Amount
+                    var amount = dataReader[2].ToString();
+
+                    // Description
+                    var description = dataReader[3].ToString();
+
+                    // Add each to list
+                    tempList.Add(date);
+                    tempList.Add(type);
+                    tempList.Add(amount);
+                    tempList.Add(description);
+
+                    // Add to list of lists
+                    returnList.Add(tempList);
+
+                    // Old code
                     valuesList.Add((string)dataReader[0]);
                 }
                 connection.Close();
